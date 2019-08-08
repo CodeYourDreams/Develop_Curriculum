44deda1ab069fde7e95bf7fd116d52e75df575c2
apouba <51918954+apouba@users.noreply.github.com>
2019-08-08 17:41:07 -0500

    Add files via upload

--- Chat Applications with Flask/Messaging/chat_forms.py
+++ Chat Applications with Flask/Messaging/chat_forms.py
@@ -1,10 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField, TextAreaField, validators
+from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username')
+    username = StringField('Username: ')
     submit = SubmitField('Login!')
 
 class MessageForm(FlaskForm):
-    message = TextAreaField('Message')
+    recipient = StringField(u"Recipient: ", validators=[DataRequired()])
+    body = TextAreaField(u'Message', validators=[DataRequired()])
     submit = SubmitField('Send')
+
+class PostForm(FlaskForm):
+    post = TextAreaField('Say something')
+    submit = SubmitField('Submit')
--- Chat Applications with Flask/Messaging/chat_site.py
+++ Chat Applications with Flask/Messaging/chat_site.py
@@ -1,9 +1,11 @@
-import os, datetime
+import os, datetime, getpass
 from chat_forms import LoginForm, MessageForm
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import UserMixin, current_user, LoginManager, login_required
+from flask_login import UserMixin, current_user, LoginManager, login_required, login_user, \
+AnonymousUserMixin, fresh_login_required
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
@@ -16,22 +18,25 @@ app.config[ 'SECRET_KEY' ] = 'jsbcfsbfjefebw237u3gdbdc' # encrypts messages
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['POSTS_PER_PAGE'] = 30
 
 db = SQLAlchemy(app)
 Migrate(app,db)
 login = LoginManager()
 login.init_app(app)
+bootstrap = Bootstrap(app)
 
 
 ##########################
 ### MODELS#################
 #####################
 
-class User(UserMixin, db.Model):
+class User(UserMixin, db.Model, AnonymousUserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, index=True, unique=True, default="Guest")
     last_seen = db.Column(db.DateTime, default = datetime.datetime.now())
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     messages_sent = db.relationship('Message',
                                     foreign_keys='Message.sender_id',
@@ -39,7 +44,7 @@ class User(UserMixin, db.Model):
     messages_received = db.relationship('Message',
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
-    last_message_read_time = db.Column(db.DateTime)
+    last_message_read_time = db.Column(db.DateTime, default = datetime.datetime.now())
 
     def is_authenticated(self):
         return True
@@ -58,19 +63,46 @@ class User(UserMixin, db.Model):
     def __init__(self,username):
         self.username = username
 
+    # def to_dict(self):
+    #     data = {
+    #         'id': self.id,
+    #         'username': self.username,
+    #         'last_seen': self.last_seen.isoformat() + 'Z',
+    #     }
+    #     return data
+    #
+    # def from_dict(self, data):
+    #     for field in ['username']:
+    #         if field in data:
+    #             setattr(self, field, data[field])
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+# @login.unauthorized_handler
+# def unauthorized():
+#
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.now())
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+
+
+class Post(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.datetime.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return 'Post {}>'.format(self.body)
 
 ############################
 ####### ROUTES ############
@@ -87,16 +119,24 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if current_user.is_authenticated:
-            current_user.last_seen = datetime.utcnow()
-            db.session.commit()
+            current_user.last_seen = datetime.datetime.now()
+
+        # "should not use login_user if user is None. And when you're creating
+        # a user, this newly created user is never assigned to the user variable"
+        # login_user(user,remember=form.username.data)
+
 
         if user is None:
-            flash('Invalid username')
-            return redirect(url_for('login'))
+            user = User(username=form.username.data)
+            # , user.last_seen = datetime.datetime
+            # flash('Invalid username')
+            # return redirect(url_for('login'))
         db.session.add(user)
         db.session.commit()
 
-        login_user(user,remember=form.remember_me.data)
+        login_user(user, force=True, fresh=True)
+        flash('logged in successfully')
+
         return redirect(url_for('chat'))
     return render_template('cs_login.html', title='Sign In', form=form)
 
@@ -105,6 +145,7 @@ def signup():
     pass
 
 @app.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
 def send_message(recipient):
     user = User.query.filter_by(username=recipient).first_or_404()
     form = MessageForm()
@@ -113,19 +154,33 @@ def send_message(recipient):
                       body=form.message.data)
         db.session.add(msg)
         db.session.commit()
-        flash(_('Your message has been sent.'))
+        # flash('Your message has been sent.')
         return redirect(url_for('main.user', username=recipient))
-    return render_template('send_message.html', title=_('Send Message'),
+    return render_template('send_message.html', title=('Send Message'),
                            form=form, recipient=recipient)
 
-@app.route('/messages')
+
+@app.route('/messages', methods=['GET', 'POST'])
+@login_required  # without login ?
 def chat():
-    current_user.last_message_read_time = datetime.datetime
+
+    current_user.last_message_read_time = datetime.datetime.now()
     db.session.commit()
+
+    form = MessageForm()
+    message = 'form not working'
+    if form.validate_on_submit():
+        user = current_user
+        message = Message(author=current_user,
+                      recipient=form.recipient.data,
+                      msg=form.body.data)
+        db.session.add(message.format)
+        db.session(commit)
+
+    print(message)
 
     page = request.args.get('page', 1, type=int)
     messages = current_user.messages_received.order_by(
-    #messages = current_user.messages_received.order_by(
         Message.timestamp.desc()).paginate(
             page, current_app.config['POSTS_PER_PAGE'], False)
     next_url = url_for('main.messages', page=messages.next_num) \
@@ -133,16 +188,9 @@ def chat():
     prev_url = url_for('main.messages', page=messages.prev_num) \
         if messages.has_prev else None
 
-    return render_template('messages.html', messages=messages.items,
+    return render_template('messages.html', messages=messages.items, form=form,
                            next_url=next_url, prev_url=prev_url)
-    # form = MessageForm()
-    #
-    # if form.validate_on_submit():
-    #     message = Message(message=form.message.data)
-    #     db.session.add(message)
-    #     db.session(commit)
-    #
-    # return render_template('cs_login.html')
+
 
 if __name__ == '__main__':
   app.run(debug = True) #opens debug traceback
